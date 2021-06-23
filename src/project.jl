e3cfd953f0620c15d7d49e4c6244683ceedeb1b2
Zongyu Li <54018575+ZongyuLi-umich@users.noreply.github.com>
2021-06-23 11:08:39 -0400

    proj v2

--- src/project.jl
+++ src/project.jl
@@ -4,10 +4,9 @@ using ImageTransformations
 using ImageFiltering
 using OffsetArrays
 using FFTW
-# reale = (x) -> (@assert x ≈ real(x); real(x))
-# myfft = x -> fftshift(fft(ifftshift(x)))
-# myifft = x -> fftshift(ifft(ifftshift(x)))
-mypad = (x, p) -> padarray(x, Fill(0, (0, 0), p .- size(x)))
+# myzeropad1 = (x, p) -> padarray(x, Fill(0, (0, 0), (p - size(x, 1), 0))) # zero pad for x
+# myreplicatepad2 = (x, p) -> padarray(x, Pad(:replicate, (0, 0), (0, p - size(x, 2)))) # replicate pad for z
+Power2 = x -> Int(exp2(Int(ceil(log2(x)))))
 """
     SPECTplan
 
@@ -16,58 +15,76 @@ Struct for storing key factors for a SPECT system model
 - `psfs [n,n,ny,nview]` usually 4D, but could be 3D for a circular orbit
 - `nview` number of views, must be integer
 - `interphow` Interpolation methods, default is bilinear interpolation
+- `viewangle` a vector of angles ranging from 0 to 2π
+- `dy` voxel size in y direction
+- `nx` number of voxels in x direction of the image, must be integer
+- `ny` number of voxels in y direction of the image, must be integer
+- `nz` number of voxels in z direction of the image, must be integer
+- `nx_psf` number of voxels in x direction of the psf, must be integer
+- `nz_psf` number of voxels in z direction of the psf, must be integer
+- `pad[up,down,left,right]` number of padding voxels in each direction, must be integer
+- `mypad` padding function using replicate border condition
 Currently code assumes each of the `nview` projection views is `[nx,nz]`
 Currently code assumes `nx = ny`
+Currently code assumes uniform sampling
 """
 struct SPECTplan
     mumap::AbstractArray{<:Real} # [nx,ny,nz] attenuation map, must be 3D, possibly zeros()
     psfs::AbstractArray{<:Real} # PSFs could be 3D or 4D
     nview::Int
-    # work_fft::AbstractMatrix
     interphow::BSpline{<:Any}
+    viewangle::AbstractVector
+    dy::Float32
+    nx::Int
+    ny::Int
+    nz::Int
+    nx_psf::Int
+    nz_psf::Int
+    padup::Int
+    paddown::Int
+    padleft::Int
+    padright::Int
+    mypad::Function
     # other options for how to do the projection?
-    function SPECTplan(mumap, psfs, nview::Int; interpidx::Int = 2)
+    function SPECTplan(mumap, psfs, nview, dy; interpidx::Int = 1)
         # check nx = ny ? typically 128 x 128 x 81
         nx, ny, nz = size(mumap)
+        nx_psf = size(psf, 1)
+        nz_psf = size(psf, 2)
         @assert isequal(nx, ny)
-        # work_fft = zeros(T, size?)
-        if interpidx === 1
+        if interpidx === 0
             interphow = BSpline(Constant()) # nearest neighbor interpolation
-        elseif interpidx === 2
+        elseif interpidx === 1
             interphow = BSpline(Linear()) # (multi)linear interpolation
         elseif interpidx === 3
             interphow = BSpline(Cubic(Line(OnGrid()))) # cubic b-spline interpolation
         else
             throw("unknown interpidx!")
         end
-        new(mumap, psfs, nview, interphow) #  creates objects of the block's type (inner constructor methods).
+        viewangle = (0:nview-1) / nview * (2π)
+        padleft = Int(ceil((Power2(nx+nx_psf-1) - nx) / 2))
+        padright = Int(floor((Power2(nx+nx_psf-1) - nx) / 2))
+        padup = Int(ceil((Power2(nz+nz_psf-1) - nz) / 2))
+        paddown = Int(floor((Power2(nz+nz_psf-1) - nz) / 2))
+        mypad = x -> padarray(x, Pad(:replicate, (padleft, padup), (padright, paddown)))
+        new(mumap, psfs, nview, interphow, viewangle, dy, nx, ny, nz, nx_psf, nz_psf,
+            padup, paddown, padleft, padright, mypad)
+        #  creates objects of the block's type (inner constructor methods).
     end
 end
 
 
 """
-    my_conv(img, ker)
+    my_conv(img, ker, plan)
     Convolve an image with a kernel using FFT with zero padding
 """
-function my_conv(img, ker)
-    nx, nz = size(img)
-    # p, q = size(ker)
-    px = 2 * nx
-    pz = 2 * nz
-    img_padded = mypad(img, (px, pz))
-    # kernel is already normalized
-    w = centered(reverse(ker))
-    # ker_padded = freqkernel(centered(ker), (px, pz))
-    # blurred_img_padded = reale(myifft(myfft(img_padded) .* ker_padded))
-    # blurred_img = blurred_img_padded[1:nx, 1:nz]
-    blurred_img_padded = imfilter(img_padded, w, Fill(0, w), Algorithm.FFT())
-    blurred_img = blurred_img_padded[1:nx, 1:nz]
-    return blurred_img
+function my_conv(img, ker, plan)
+    return max.(0, imfilter(plan.mypad(img), centered(reverse(ker)), Algorithm.FFT()))[1:plan.nx, 1:plan.nz]
 end
 
 """
-    1. project!(view, plan, image, viewidx)
-    Project a single view
+    project!(view, plan, image, viewidx)
+    1. Project a single view
 """
 function project!(
     view::AbstractMatrix{<:AbstractFloat},
@@ -76,45 +93,27 @@ function project!(
     viewidx::Int,
 )
     # todo : read multiple dispatch
-
-    # rotate image, image is in nx * ny * nz, typically 128 x 128 x 81
-    # mumap, psfs, nview, work_fft, interphow = SPECTplan(mumap, psfs, plan.nview, plan.interpidx)
-    θ = - 2π / plan.nview * viewidx
     rotate = x -> OffsetArrays.no_offset_view(
-                            imrotate(x, θ, axes(x), 0, # details check the rotation center
+                            imrotate(x, - plan.viewangle[viewidx], axes(x), 0, # details check the rotation center
                             method = plan.interphow))
+    # rotate image
     imgr = mapslices(rotate, image, dims = [1, 2])
     # rotate mumap
     mumapr = mapslices(rotate, plan.mumap, dims = [1, 2])
-    # running sum and exp? of mumap
-    # view = zeros(eltype(image), nx, nz) # one projection view
-    # No loop is actually needed, maybe using cumsum and mapslices
-    # temporary variable, should be part of the plan
-    # px = 2 * nx
-    # pz = 2 * nz
-    # exp_mumapr = exp.(reverse(-cumsum(reverse(mumapr; dims = 2), dims = 2), dims=2)) # nx * ny * nz
-    # freqpsf = permutedims(mapslices(x -> freqkernel(centered(x), (px, pz)), plan.psfs[:,:,:,viewidx], dims = [1,2]), [1,3,2]) # px * pz * ny -> px * ny * pz
-    # freq_img_padded = mapslices(x -> myfft(mypad(x, (px, pz))), imgr, dims = [1,3]) # px * ny * pz
-    # blurred_imgr = mapslices(x -> reale(myifft(x))[1:nx, 1:nz], freq_img_padded .* freqpsf, dims = [1,3]) # nx * ny * nz
-    # w = centered(reverse(ker))
-    # blurred_imgr = mapslices(x -> imfilter)
-    # view = dropdims(sum(blurred_imgr .* exp_mumapr, dims = 2); dims = 2)
-
-    for i = 1:ny
-        exp_mumapr = dropdims(exp.(-sum(mumapr[:, 1:i, :], dims = 2)); dims = 2)
-        blurred_imgr = my_conv(imgr[:, i, :], plan.psfs[:, :, i, viewidx])
-        view += blurred_imgr .* exp_mumapr
-    end
     # loop over image planes
         # use zero-padded fft (because big) or conv (if small) to convolve with psf
         # sum, account for mumap
+    for i = 1:ny
+        exp_mumapr = dropdims(exp.(-sum(plan.dy * mumapr[:, 1:i, :], dims = 2)); dims = 2) # nx * nz
+        view += my_conv(imgr[:, i, :] .* exp_mumapr, plan.psfs[:, :, i, viewidx], plan)
+    end
     return view
 end
 
 
 """
-    2. project!(views, plan, image ; index)
-    Project multiple views, call 1
+    project!(views, plan, image ; index)
+    2. Project multiple views, call 1
 """
 function project!(
     views::AbstractArray{<:AbstractFloat,3},
@@ -125,6 +124,7 @@ function project!(
 
     # loop over each view index
     for i in index
+        # project!((@view views[:,:,i]), plan, image, i) # this line doesn't work, the output is all zero
         views[:,:,i] = project!(views[:,:,i], plan, image, i)
     end
     return views
@@ -132,8 +132,8 @@ end
 
 
 """
-    3. views = project(plan, image ; index)
-    Initialize views, call 2
+    views = project(plan, image ; index)
+    3. Initialize views, call 2
 """
 function project(
     plan::SPECTplan,
@@ -142,7 +142,7 @@ function project(
 )
     nx = size(image, 1)
     nz = size(image, 3)
-    views = zeros(eltype(image), nx, nz, plan.nview)
+    views = zeros(promote_type(eltype(image), Float32), nx, nz, plan.nview)
     return project!(views, plan, image ; kwargs...)
 end
 
@@ -154,10 +154,11 @@ function project(
     image::AbstractArray{<:Real,3},
     mumap::AbstractArray{<:Real}, # [nx,ny,nz] attenuation map, must be 3D, possibly zeros()
     psfs::AbstractArray{<:Real},
-    nview::Int;
-    interpidx::Int = 2,
+    nview::Int,
+    dy::Float32;
+    interpidx::Int = 1,
     kwargs...,
 )
-    plan = SPECTplan(mumap, psfs, nview; interpidx = interpidx)
-    project(plan, image ; kwargs...)
+    plan = SPECTplan(mumap, psfs, nview, dy; interpidx = interpidx)
+    project(plan, image; kwargs...)
 end
