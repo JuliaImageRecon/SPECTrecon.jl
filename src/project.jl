d2ce120b24ae8915679d3b9a452c748cca3501d0
Zongyu Li <54018575+ZongyuLi-umich@users.noreply.github.com>
2021-07-13 21:24:00 -0400

    adj, 1d and 2d

--- src/project.jl
+++ src/project.jl
@@ -1,6 +1,7 @@
 # project.jl
-using Interpolations, ImageTransformations, ImageFiltering, OffsetArrays, LinearAlgebra
+# Use BorderArray instead of PadArray
 include("rotate3.jl")
+using Interpolations, ImageTransformations, ImageFiltering, OffsetArrays, LinearAlgebra, FFTW
 """
     SPECTplan
 
@@ -8,15 +9,17 @@ Struct for storing key factors for a SPECT system model
 - `mumap [nx,ny,nz]` attenuation map, must be 3D, possibly zeros()
 - `psfs [nx_psf,nz_psf,ny,nview]` point spread function, must be 4D, with `nx_psf` and `nz_psf` odd, and symmetric for each slice
 - `nview` number of views, must be integer
-- `interphow` Interpolation method, default is bilinear interpolation
-- `viewangle` a vector of angles ranging from 0 to 2π
+- `rotateforw` rotation method, default is using 1d linear interpolation
+- `rotateadjt` adjoint of rotation method, default is using 1d linear interpolation
 - `dy` voxel size in y direction (dx is the same value)
 - `nx` number of voxels in x direction of the image, must be integer
 - `ny` number of voxels in y direction of the image, must be integer
 - `nz` number of voxels in z direction of the image, must be integer
 - `nx_psf` number of voxels in x direction of the psf, must be integer
 - `nz_psf` number of voxels in z direction of the psf, must be integer
-- `mypad` padding function using replicate border condition
+- `padrepl` padding function using replicate border condition
+- `padzero` padding function using zero border condition
+- {padleft,padright,padup,paddown} pixels padded along {left,right,up,down} direction
 - `alg` algorithms used for convolution, default is FFT
 Currently code assumes each of the `nview` projection views is `[nx,nz]`
 Currently code assumes `nx = ny`
@@ -25,9 +28,10 @@ Currently imrotate3 code only supports linear interpolation
 """
 struct SPECTplan
     mumap::AbstractArray{<:Real, 3} # [nx,ny,nz] attenuation map, must be 3D, possibly zeros()
-    psfs::AbstractArray{<:Real, 4} # PSFs must be 4D, [nx_psf, nz_psf, ny, nview]
+    psfs::AbstractArray{<:Real, 4} # PSFs must be 4D, [nx_psf, nz_psf, ny, nview], finally be centered psf
     nview::Int
-    interphow::BSpline{<:Any}
+    rotateforw::Function
+    rotateadjt::Function
     viewangle::AbstractVector
     dy::Float32
     nx::Int
@@ -35,7 +39,12 @@ struct SPECTplan
     nz::Int
     nx_psf::Int
     nz_psf::Int
-    mypad::Function
+    padrepl::Function
+    padzero::Function
+    padleft::Int
+    padright::Int
+    padup::Int
+    paddown::Int
     alg::Any
     # other options for how to do the projection?
     function SPECTplan(mumap,
@@ -55,15 +64,16 @@ struct SPECTplan
         @assert isequal(nx, ny)
         @assert iseven(nx) && iseven(ny)
         @assert isodd(nx_psf) && isodd(nz_psf)
-        @assert all(mapslices(issymmetric, psfs, dims = [1, 2]))
-        if interpidx == 0
-            interphow = BSpline(Constant()) # nearest neighbor interpolation
-        elseif interpidx == 1
-            interphow = BSpline(Linear()) # (multi)linear interpolation
-        elseif interpidx == 3
-            interphow = BSpline(Cubic(Line(OnGrid()))) # cubic b-spline interpolation
+        issym = x -> x == reverse(x)
+        @assert all(mapslices(issym, psfs, dims = [1, 2]))
+        if interpidx == 1
+            rotateforw = imrotate3
+            rotateadjt = imrotate3_adj
+        elseif interpidx == 2
+            rotateforw = imrotate3emmt
+            rotateadjt = imrotate3emmt_adj
         else
-            throw("unknown interpidx!")
+            throw("invalid interpidx!")
         end
         viewangle = (0:nview-1) / nview * (2π)
 
@@ -74,7 +84,11 @@ struct SPECTplan
         padup = iszero(padup) ? ceil(Int, (Power2(nz+nz_psf-1) - nz) / 2) : padup
         paddown = iszero(paddown) ? floor(Int, (Power2(nz+nz_psf-1) - nz) / 2) : paddown
 
-        mypad = x -> padarray(x, Pad(:replicate, (padleft, padup), (padright, paddown)))
+        # mypad1 = x -> padarray(x, Pad(:replicate, (nx_psf, nz_psf), (nx_psf, nz_psf)))
+        padrepl = x -> padarray(x, Pad(:replicate, (padleft, padup), (padright, paddown)))
+        padzero = x -> padarray(x, Fill(0, (padleft, padup), (padright, paddown)))
+
+        # mypad = x -> mypad2(mypad1(x))
 
         if conv_alg === :fft
             alg = Algorithm.FFT()
@@ -83,33 +97,40 @@ struct SPECTplan
         else
             throw("unknown convolution algorithm choice!")
         end
-        new(mumap, psfs, nview, interphow, viewangle, dy, nx, ny, nz,
-                nx_psf, nz_psf, mypad, alg)
+        new(mumap, psfs, nview, rotateforw, rotateadjt, viewangle, dy, nx, ny, nz,
+                nx_psf, nz_psf, padrepl, padzero, padleft, padright, padup, paddown, alg)
         #  creates objects of the block's type (inner constructor methods).
     end
 end
-
 
 """
     my_conv(img, ker, plan)
     Convolve an image with a kernel
 """
 function my_conv(img, ker, plan)
-    return max.(0, imfilter(plan.mypad(img), centered(ker), plan.alg))[1:plan.nx, 1:plan.nz]
+    return max.(0, imfilter(plan.padrepl(img), centered(ker), plan.alg))[1:plan.nx, 1:plan.nz]
 end
-
+"""
+    my_conv_adj(img, ker, plan)
+"""
+function my_conv_adj(img, ker, plan)
+    tmp = imfilter(plan.padzero(img), centered(ker), plan.alg)
+    tmp[1:1, :] .+= sum(tmp[1 - plan.padleft:0, :], dims = 1)
+    tmp[plan.nx:plan.nx, :] .+= sum(tmp[plan.nx + 1:end, :], dims = 1)
+    tmp[:, 1:1] .+= sum(tmp[:, 1-plan.padup:0], dims = 2)
+    tmp[:, plan.nz:plan.nz] .+= sum(tmp[:, plan.nz+1:end], dims = 2)
+    tmp = tmp[1:plan.nx, 1:plan.nz]
+    return max.(tmp, 0)
+end
 """
     my_conv!(output, img, ker, plan)
     Convolve an image with a kernel using in-place operation
     This part is still under construction
+    centered(img) should be preallocated, get rid of if
 """
 function my_conv!(output, img, ker, plan)
-    if issymmetric(ker)
-        imfilter!(plan.workimg, plan.mypad(img), centered(ker), plan.alg)
-        return map!(output, x -> max.(x, 0), @view plan.workimg[1:plan.nx, 1:plan.nz])
-    else
-        throw("psf is not symmetric!")
-    end
+    imfilter!(plan.workimg, plan.mypad!(img), centered(ker), plan.alg)
+    return map!(output, x -> max.(x, 0), @view plan.workimg[1:plan.nx, 1:plan.nz])
 end
 
 """
@@ -124,11 +145,13 @@ function project!(
 )
     # todo : read multiple dispatch
     # rotate = x -> my_rotate(x, plan.viewangle[viewidx], plan.interphow)
-    rotate = x -> imrotate3(x, plan.viewangle[viewidx], plan.nx, plan.ny)
+    # myrotate = x -> imrotate3emmt(x, plan.viewangle[viewidx], plan.nx, plan.ny; mode = :forward)
     # rotate image
-    imgr = mapslices(rotate, image, dims = [1, 2])
+    imgr = mapslices(x -> plan.rotateforw(x, plan.viewangle[viewidx], plan.nx, plan.ny),
+                    image, dims = [1, 2])
     # rotate mumap
-    mumapr = mapslices(rotate, plan.mumap, dims = [1, 2])
+    mumapr = mapslices(x -> plan.rotateforw(x, plan.viewangle[viewidx], plan.nx, plan.ny),
+                    plan.mumap, dims = [1, 2])
     # loop over image planes
         # use zero-padded fft (because big) or conv (if small) to convolve with psf
         # sum, account for mumap
@@ -186,5 +209,92 @@ function project(
     kwargs...,
 )
     plan = SPECTplan(mumap, psfs, nview, dy; interpidx)
+    @assert minimum(image) >= 0 # image must be nonnegative
     project(plan, image; kwargs...)
+end
+
+"""
+    backproject!(view, plan, viewidx)
+    1. Backproject a single view
+"""
+function backproject!(
+    view::AbstractMatrix{<:AbstractFloat},
+    plan::SPECTplan,
+    viewidx::Int
+)
+
+    # todo : read multiple dispatch
+    # rotate = x -> my_rotate(x, plan.viewangle[viewidx], plan.interphow)
+
+    # myrotate = x -> imrotate3emmt(x, plan.viewangle[viewidx], plan.nx, plan.ny; mode = :forward)
+    # myderotate = x -> imrotate3emmt(x, plan.viewangle[viewidx], plan.nx, plan.ny; mode = :adjoint)
+
+    # rotate mumap
+    mumapr = mapslices(x -> plan.rotateforw(x, plan.viewangle[viewidx], plan.nx, plan.ny),
+                    plan.mumap, dims = [1, 2])
+
+    # adjoint of sum along y axis
+    imgr = repeat(reshape(view, nx, 1, nz), 1, ny, 1)
+
+    for i = 1:plan.ny
+        exp_mumapr = dropdims(exp.(-plan.dy*(sum(mumapr[:, 1:i, :], dims = 2) .- (mumapr[:,i:i,:]/2))); dims = 2) # nx * nz
+        # adjoint of convolution, convolve with reverse of psfs
+        # adjoint of multiplying with mumap
+        imgr[:, i, :] = my_conv_adj(imgr[:, i, :], plan.psfs[:, :, i, viewidx], plan) .* exp_mumapr
+    end
+    # adjoint of imrotate
+    image = mapslices(x -> plan.rotateadjt(x, plan.viewangle[viewidx], plan.nx, plan.ny),
+                    imgr, dims = [1, 2])
+
+    return image
+end
+
+
+"""
+    backproject!(views, plan, image ; index)
+    2. Backproject multiple views, call 1
+"""
+function backproject!(
+    views::AbstractArray{<:AbstractFloat,3},
+    plan::SPECTplan,
+    image::AbstractArray{<:Real,3} ;
+    index::AbstractVector{<:Int} = 1:plan.nview, # all views
+)
+
+    # loop over each view index
+    for i in index
+        image .+= backproject!(views[:,:,i], plan, i)
+    end
+    return image
+end
+
+"""
+    image = backproject(plan, views ; index)
+    3. Initialize image, call 2
+"""
+function backproject(
+    plan::SPECTplan,
+    views::AbstractArray{<:Real,3} ;
+    kwargs...,
+)
+    image = zeros(promote_type(eltype(views), Float32), plan.nx, plan.ny, plan.nz)
+    return backproject!(views, plan, image ; kwargs...)
+end
+
+
+"""
+    image = backproject(views, mumap, psfs, nview, interpidx; kwargs...), test the function, call 3
+"""
+function backproject(
+    views::AbstractArray{<:Real,3},
+    mumap::AbstractArray{<:Real}, # [nx,ny,nz] attenuation map, must be 3D, possibly zeros()
+    psfs::AbstractArray{<:Real},
+    nview::Int,
+    dy::Float32;
+    interpidx::Int = 1,
+    kwargs...,
+)
+    plan = SPECTplan(mumap, psfs, nview, dy; interpidx)
+    @assert minimum(views) >= 0 # views must be nonnegative
+    backproject(plan, views; kwargs...)
 end
--- src/rotate3.jl
+++ src/rotate3.jl
@@ -1,30 +1,81 @@
+using LazyAlgebra, TwoDimensional
+using LinearInterpolators
+using InterpolationKernels
 """
     rotate_x(img, θ, M, N)
-    rotate an image along x axis in clockwise direction using linear interpolation
+    rotate an image along x axis in clockwise direction using 1d linear interpolation
 """
 function rotate_x(img, θ, M, N)
-    xi = -(M-1)/2 : (M-1)/2
-    yi = -(N-1)/2 : (N-1)/2
-    rotate_x(xin, yin, θ) = xin + yin * tan(θ/2)
+    # xi = -(M-1)/2 : (M-1)/2
+    # yi = -(N-1)/2 : (N-1)/2
+    xi = 1:M
+    yi = 1:N
+    rotate_x(xin, yin, θ) = xin + (yin - (N+1)/2) * tan(θ/2)
     tmp = zeros(eltype(img), M, N)
     for (i, yin) in enumerate(yi)
-        ic = LinearInterpolation(xi, img[:, i], extrapolation_bc = 0)
-        tmp[:, i] .= ic.(rotate_x.(xi, yin, θ))
+        I1 = SparseInterpolator(LinearSpline(Float32), rotate_x.(xi, yin, θ), M)
+        # ic = LinearInterpolation(xi, img[:, i], extrapolation_bc = 0)
+        # tmp[:, i] .= ic.(rotate_x.(xi, yin, θ))
+        tmp[:, i] .= I1 * img[:, i]
     end
     return tmp
 end
 """
+    rotate_x_adj(img, θ, M, N)
+    The adjoint of rotating an image along x axis in clockwise direction using 1d linear interpolation
+"""
+function rotate_x_adj(img, θ, M, N)
+    # xi = -(M-1)/2 : (M-1)/2
+    # yi = -(N-1)/2 : (N-1)/2
+    xi = 1:M
+    yi = 1:N
+    rotate_x(xin, yin, θ) = xin + (yin - (N+1)/2) * tan(θ/2)
+    tmp = zeros(eltype(img), M, N)
+    for (i, yin) in enumerate(yi)
+        I1 = SparseInterpolator(LinearSpline(Float32), rotate_x.(xi, yin, θ), M)
+        # ic = LinearInterpolation(xi, img[:, i], extrapolation_bc = 0)
+        # tmp[:, i] .= ic.(rotate_x.(xi, yin, θ))
+        tmp[:, i] .= I1' * img[:, i]
+    end
+    return tmp
+end
+
+"""
     rotate_y(img, θ, M, N)
-    rotate an image along y axis in clockwise direction using linear interpolation
+    rotate an image along y axis in clockwise direction using 1d linear interpolation
 """
 function rotate_y(img, θ, M, N)
-    xi = -(M-1)/2 : (M-1)/2
-    yi = -(N-1)/2 : (N-1)/2
-    rotate_y(xin, yin, θ) = xin * (-sin(θ)) + yin
+    # xi = -(M-1)/2 : (M-1)/2
+    # yi = -(N-1)/2 : (N-1)/2
+    xi = 1:M
+    yi = 1:N
+    rotate_y(xin, yin, θ) = (xin - (M+1)/2) * (-sin(θ)) + yin
     tmp = zeros(eltype(img), M, N)
     for (i, xin) in enumerate(xi)
-        ic = LinearInterpolation(yi, img[i, :], extrapolation_bc = 0)
-        tmp[i, :] .= ic.(rotate_y.(xin, yi, θ))
+        I1 = SparseInterpolator(LinearSpline(Float32), rotate_y.(xin, yi, θ), N)
+        tmp[i, :] .= I1 * img[i, :]
+        # ic = LinearInterpolation(yi, img[i, :], extrapolation_bc = 0)
+        # tmp[i, :] .= ic.(rotate_y.(xin, yi, θ))
+    end
+    return tmp
+end
+
+"""
+    rotate_y_adj(img, θ, M, N)
+    The adjoint of rotating an image along y axis in clockwise direction using 1d linear interpolation
+"""
+function rotate_y_adj(img, θ, M, N)
+    # xi = -(M-1)/2 : (M-1)/2
+    # yi = -(N-1)/2 : (N-1)/2
+    xi = 1:M
+    yi = 1:N
+    rotate_y(xin, yin, θ) = (xin - (M+1)/2) * (-sin(θ)) + yin
+    tmp = zeros(eltype(img), M, N)
+    for (i, xin) in enumerate(xi)
+        I1 = SparseInterpolator(LinearSpline(Float32), rotate_y.(xin, yi, θ), N)
+        tmp[i, :] .= I1' * img[i, :]
+        # ic = LinearInterpolation(yi, img[i, :], extrapolation_bc = 0)
+        # tmp[i, :] .= ic.(rotate_y.(xin, yi, θ))
     end
     return tmp
 end
@@ -45,9 +96,27 @@ function rot_f90(img, m)
         throw("invalid m!")
     end
 end
+
+"""
+    rot_f90_adj(img, m)
+    The adjoint of rotating an image by 90/180/270 degrees
+"""
+function rot_f90_adj(img, m)
+    if m == 0
+        return img
+    elseif m == 1
+        return rotr90(img)
+    elseif m == 2
+        return rot180(img)
+    elseif m == 3
+        return rotl90(img)
+    else
+        throw("invalid m!")
+    end
+end
 """
     imrotate3(img, θ, M, N)
-    rotate an image by angle θ (must be ranging from 0 to 2π) in clockwise direction using linear interpolation
+    rotate an image by angle θ (must be ranging from 0 to 2π) in clockwise direction using a series of 1d linear interpolation
 """
 function imrotate3(img, θ, M, N)
     m = mod(floor(Int, 0.5 + θ/(π/2)), 4)
@@ -60,7 +129,22 @@ function imrotate3(img, θ, M, N)
                 mod_theta, M_pad, N_pad), mod_theta, M_pad, N_pad), mod_theta, M_pad, N_pad)[pad_x + 1 : pad_x + M, pad_y + 1 : pad_y + N]
 end
 """
-    imrotate3jl(image, θ, plan)
+    imrotate3_adj(img, θ, M, N)
+    The adjoint of rotating an image by angle θ (must be ranging from 0 to 2π) in clockwise direction using a series of 1d linear interpolation
+"""
+function imrotate3_adj(img, θ, M, N)
+    m = mod(floor(Int, 0.5 + θ/(π/2)), 4)
+    mod_theta = θ - m * (π/2) # make sure it is between -45 and 45 degree
+    pad_x = ceil(Int, 1 + M * sqrt(2)/2 - M / 2)
+    pad_y = ceil(Int, 1 + N * sqrt(2)/2 - N / 2)
+    M_pad = M + 2 * pad_x
+    N_pad = N + 2 * pad_y
+    return rot_f90_adj(rotate_x_adj(rotate_y_adj(rotate_x_adj(OffsetArrays.no_offset_view(padarray(img, Fill(0, (pad_x, pad_y)))),
+                mod_theta, M_pad, N_pad), mod_theta, M_pad, N_pad), mod_theta, M_pad, N_pad), m)[pad_x + 1 : pad_x + M, pad_y + 1 : pad_y + N]
+end
+
+"""
+    imrotate3jl(img, θ, plan)
     Rotate an image by angle θ in counter clockwise direction using "Julia" built-in imrotate function
     Not used in this version
 """
@@ -75,5 +159,56 @@ function imrotate3jl(img, θ, interphow)
                                             axes(img), # crop the image
                                             0, # extrapolation_bc = 0
                                             method = interphow))
+    end
+end
+"""
+    imrotate3emmt(img, θ, M, N)
+    Rotate an image by angle θ in counter clockwise direction using 2d linear interpolation
+    Source code is here: https://github.com/emmt/LinearInterpolators.jl
+"""
+function imrotate3emmt(img, θ, M, N)
+    if mod(θ, 2π) ≈ 0
+        return img
+    elseif mod(θ, 2π) ≈ π
+        return rot180(img)
+    else
+        pad_x = ceil(Int, 1 + M * sqrt(2)/2 - M / 2)
+        pad_y = ceil(Int, 1 + N * sqrt(2)/2 - N / 2)
+        padded_img = OffsetArrays.no_offset_view(padarray(img, Fill(0, (pad_x, pad_y))))
+        ker = LinearSpline(Float32)
+        M_pad = M + 2 * pad_x
+        N_pad = N + 2 * pad_y
+        rows = (M_pad, N_pad)
+        cols = (M_pad, N_pad)
+        c = ((1 + M_pad) / 2, (1 + N_pad) / 2)
+        R = c + rotate(2π - θ, AffineTransform2D{Float32}() - c)
+        A = TwoDimensionalTransformInterpolator(rows, cols, ker, R)
+        return (A * padded_img)[pad_x + 1 : pad_x + M, pad_y + 1 : pad_y + N]
+    end
+end
+
+"""
+    imrotate3emmt_adj(img, θ, M, N)
+    The adjoint of rotating an image by angle θ in counter clockwise direction using 2d linear interpolation
+    Source code is here: https://github.com/emmt/LinearInterpolators.jl
+"""
+function imrotate3emmt_adj(img, θ, M, N)
+    if mod(θ, 2π) ≈ 0
+        return img
+    elseif mod(θ, 2π) ≈ π
+        return rot180(img)
+    else
+        pad_x = ceil(Int, 1 + M * sqrt(2)/2 - M / 2)
+        pad_y = ceil(Int, 1 + N * sqrt(2)/2 - N / 2)
+        padded_img = OffsetArrays.no_offset_view(padarray(img, Fill(0, (pad_x, pad_y))))
+        ker = LinearSpline(Float32)
+        M_pad = M + 2 * pad_x
+        N_pad = N + 2 * pad_y
+        rows = (M_pad, N_pad)
+        cols = (M_pad, N_pad)
+        c = ((1 + M_pad) / 2, (1 + N_pad) / 2)
+        R = c + rotate(2π - θ, AffineTransform2D{Float32}() - c)
+        A = TwoDimensionalTransformInterpolator(rows, cols, ker, R)
+        return (A' * padded_img)[pad_x + 1 : pad_x + M, pad_y + 1 : pad_y + N]
     end
 end
