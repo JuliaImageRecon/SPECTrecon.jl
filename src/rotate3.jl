1bd909176d6890298bd971a9b78a7a6967373ff0
Zongyu Li <54018575+ZongyuLi-umich@users.noreply.github.com>
2021-07-15 15:29:33 -0400

    Update src/rotate3.jl
    Co-authored-by: Jeff Fessler <JeffFessler@users.noreply.github.com>

--- src/rotate3.jl
+++ src/rotate3.jl
@@ -170,7 +170,7 @@ function imrotate3emmt!(output, img, θ, M, N, pad_x, pad_y)
     elseif mod(θ, 2π) ≈ π
         return rot180(img)
     else
-        output = OffsetArrays.no_offset_view(BorderArray(img, Fill(0, (pad_x, pad_y))))
+        output .= OffsetArrays.no_offset_view(BorderArray(img, Fill(0, (pad_x, pad_y))))
         ker = LinearSpline(Float32)
         M_pad = M + 2 * pad_x
         N_pad = N + 2 * pad_y
