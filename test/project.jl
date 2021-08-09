9442707310b54ad8c11c54ea21c592827fd4ef60
Zongyu Li <54018575+ZongyuLi-umich@users.noreply.github.com>
2021-08-09 15:43:53 -0400

    tweaks

--- test/project.jl
+++ test/project.jl
@@ -9,16 +9,19 @@ using LinearMapsAA
     mumap = convert(Array{Float32, 3}, 0.01 * rand(Float32, nx, ny, nz))
     psfs = convert(Array{Float32, 4}, 1/25 * ones(Float32, 5, 5, ny, nview))
     dy = Float32(4.7952)
+
     # 1d linear interpolation
     A = LinearMapAA(x -> SPECTrecon.project(x, mumap, psfs, nview, dy; interpidx = 1),
                     y -> SPECTrecon.backproject(y, mumap, psfs, nview, dy; interpidx = 1),
                     (nx*nz*nview, nx*ny*nz);
                     T=Float32, idim = (nx, ny, nz), odim = (nx, nz, nview))
+
     # 2d bilinear interpolation
     B = LinearMapAA(x -> SPECTrecon.project(x, mumap, psfs, nview, dy; interpidx = 2),
                     y -> SPECTrecon.backproject(y, mumap, psfs, nview, dy; interpidx = 2),
                     (nx*nz*nview, nx*ny*nz);
                     T=Float32, idim = (nx, ny, nz), odim = (nx, nz, nview))
+
     x = randn(Float32, nx, ny, nz)
     y = randn(Float32, nx, nz, nview)
     @test isapprox(vdot(y, A * x), vdot(x, A' * y))
