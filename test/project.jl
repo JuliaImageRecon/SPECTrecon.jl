3d61ba9d4be3e31ee24484e996fab82358555673
Zongyu Li <54018575+ZongyuLi-umich@users.noreply.github.com>
2021-08-11 21:01:23 -0400

    mem benchmark

--- test/project.jl
+++ test/project.jl
@@ -1,6 +1,8 @@
 using Main.SPECTrecon:project, backproject
 using Test: @test, @testset, @test_throws, @inferred
+using LinearAlgebra
 using LinearMapsAA
+using BenchmarkTools
 @testset "project" begin
     nx = 16
     ny = 16
@@ -27,3 +29,44 @@ using LinearMapsAA
     @test isapprox(vdot(y, A * x), vdot(x, A' * y))
     @test isapprox(vdot(y, B * x), vdot(x, B' * y))
 end
+
+nx = 16
+ny = 16
+nz = 16
+nview = 16
+mumap = convert(Array{Float32, 3}, 0.01 * rand(Float32, nx, ny, nz))
+psfs = convert(Array{Float32, 4}, 1/25 * ones(Float32, 5, 5, ny, nview))
+dy = Float32(4.7952)
+plan = SPECTrecon.SPECTplan(mumap, psfs, nview, dy; interpidx = 1)
+workarray = Vector{SPECTrecon.Workarray_s}(undef, plan.ncore)
+for i = 1:plan.ncore
+    workarray[i] = SPECTrecon.Workarray_s(plan) # allocate
+end
+x = randn(Float32, nx, ny, nz)
+y = randn(Float32, nx, nz, nview)
+views = zeros(Float32, nx, nz, nview)
+@btime SPECTrecon.project!(views, plan, workarray, x)
+# 1d interp, 34.702 ms (203927 allocations: 10.00 MiB)
+
+
+plan = SPECTrecon.SPECTplan(mumap, psfs, nview, dy; interpidx = 2)
+workarray = Vector{SPECTrecon.Workarray_s}(undef, plan.ncore)
+for i = 1:plan.ncore
+    workarray[i] = SPECTrecon.Workarray_s(plan) # allocate
+end
+x = randn(Float32, nx, ny, nz)
+y = randn(Float32, nx, nz, nview)
+views = zeros(Float32, nx, nz, nview)
+@btime SPECTrecon.project!(views, plan, workarray, x)
+# 2d interp, 33.247 ms (76446 allocations: 4.30 MiB)
+
+
+img = randn(Float32, plan.nx, plan.nz)
+@btime SPECTrecon.my_conv!(workarray[1].padimg,
+                  img,
+                  (@view plan.psfs[:, :, 8, 8]),
+                  workarray[1].img_compl,
+                  workarray[1].ker_compl,
+                  workarray[1].tmp_compl,
+                  plan)
+# 90.450 μs (133 allocations: 9.89 KiB)
--- test/rotate3.jl
+++ test/rotate3.jl
@@ -1,8 +1,10 @@
-# using Main.SPECTrecon:imrotate3!, imrotate3_adj!, imrotate3emmt!, imrotate3emmt_adj!
+using Main.SPECTrecon:imrotate3!, imrotate3_adj!
 using Test: @test, @testset, @test_throws, @inferred
 using OffsetArrays
 using ImageFiltering
 using LazyAlgebra
+using InterpolationKernels
+using LinearInterpolators
 using BenchmarkTools
 @testset "imrotate3" begin
     M = 16
@@ -21,37 +23,37 @@ using BenchmarkTools
     tmp_y = similar(output_y)
     θ_list = [π/7, 3π/7, 5π/7, π, 9π/7, 11π/7, 13π/7]
     for θ in θ_list
-        @test isapprox(vdot(y, imrotate3!(output_x, tmp_x, x, θ, M, N, pad_x, pad_y, A_x, A_y, vec_x, vec_y)),
-                    vdot(x, imrotate3_adj!(output_y, tmp_y, y, θ, M, N, pad_x, pad_y, A_x, A_y, vec_x, vec_y)))
-        @test isapprox(vdot(y, imrotate3!(output_x, tmp_x, x, θ, M, N, pad_x, pad_y)),
-                    vdot(x, imrotate3_adj!(output_y, tmp_y, y, θ, M, N, pad_x, pad_y)))
+        @test isapprox(vdot(y, SPECTrecon.imrotate3!(output_x, tmp_x, x, θ, M, N, pad_x, pad_y, A_x, A_y, vec_x, vec_y)),
+                    vdot(x, SPECTrecon.imrotate3_adj!(output_y, tmp_y, y, θ, M, N, pad_x, pad_y, A_x, A_y, vec_x, vec_y)))
+        @test isapprox(vdot(y, SPECTrecon.imrotate3!(output_x, tmp_x, x, θ, M, N, pad_x, pad_y)),
+                    vdot(x, SPECTrecon.imrotate3_adj!(output_y, tmp_y, y, θ, M, N, pad_x, pad_y)))
     end
 end
 
-# M = 16
-# N = 16
-# pad_x = ceil(Int, 1 + M * sqrt(2)/2 - M / 2)
-# pad_y = ceil(Int, 1 + N * sqrt(2)/2 - N / 2)
-# xi = 1 : M + 2 * pad_x
-# yi = 1 : N + 2 * pad_y
-# vec_x = zeros(Float32, M + 2 * pad_x)
-# vec_y = zeros(Float32, N + 2 * pad_y)
-# A_x = SparseInterpolator(LinearSpline(Float32), vec_x, length(vec_x))
-# A_y = SparseInterpolator(LinearSpline(Float32), vec_y, length(vec_y))
-# x = randn(Float32, M, N)
-# y = randn(Float32, M, N)
-# output_x = OffsetArrays.no_offset_view(padarray(x, Fill(0, (pad_x, pad_y))))
-# output_y = OffsetArrays.no_offset_view(padarray(y, Fill(0, (pad_x, pad_y))))
-# tmp_x = similar(output_x)
-# tmp_y = similar(output_y)
-# θ_list = [π/7, 3π/7, 5π/7, π, 9π/7, 11π/7, 13π/7]
-# @btime imrotate3_adj!(output_x, tmp_x, x, θ_list[1], M, N, pad_x, pad_y, A_x, A_y, vec_x, vec_y)
-# A_x = SparseInterpolator(LinearSpline(Float32), vec_x, length(xi))
-# A_y = SparseInterpolator(LinearSpline(Float32), vec_y, length(yi))
-# imrotate3!(output_x, tmp_x, x, θ_list[2], M, N, pad_x,
-#                 pad_y, A_x, A_y, vec_x, vec_y)
-# imrotate3emmt!(output_y, tmp_y, x, θ_list[2], M, N, pad_x, pad_y)
-#
-#
-# @btime rotate_x!(output_x, tmp_x, θ_list[1], xi, yi, A, vec_x)
-# rotate_x_adj!(output_y, tmp_x, -θ_list[1], xi, yi, vec_x)
+M = 16
+N = 16
+pad_x = ceil(Int, 1 + M * sqrt(2)/2 - M / 2)
+pad_y = ceil(Int, 1 + N * sqrt(2)/2 - N / 2)
+xi = 1 : M + 2 * pad_x
+yi = 1 : N + 2 * pad_y
+vec_x = zeros(Float32, M + 2 * pad_x)
+vec_y = zeros(Float32, N + 2 * pad_y)
+A_x = SparseInterpolator(LinearSpline(Float32), vec_x, length(vec_x))
+A_y = SparseInterpolator(LinearSpline(Float32), vec_y, length(vec_y))
+x = randn(Float32, M, N)
+y = randn(Float32, M, N)
+output_x = OffsetArrays.no_offset_view(padarray(x, Fill(0, (pad_x, pad_y))))
+output_y = OffsetArrays.no_offset_view(padarray(y, Fill(0, (pad_x, pad_y))))
+tmp_x = similar(output_x)
+tmp_y = similar(output_y)
+θ_list = [π/7, 3π/7, 5π/7, π, 9π/7, 11π/7, 13π/7]
+@btime SPECTrecon.imrotate3!(output_x, tmp_x, x, θ_list[1], M, N, pad_x, pad_y, A_x, A_y, vec_x, vec_y)
+# 1d interp, 24.366 μs (239 allocations: 11.19 KiB)
+@btime SPECTrecon.imrotate3!(output_x, tmp_x, x, θ_list[1], M, N, pad_x, pad_y)
+# 2d interp, 6.121 μs (2 allocations: 80 bytes)
+@btime SPECTrecon.imrotate3_adj!(output_y, tmp_y, y, θ_list[1], M, N, pad_x, pad_y, A_x, A_y, vec_x, vec_y)
+# 1d interp, 27.178 μs (329 allocations: 15.12 KiB)
+@btime SPECTrecon.imrotate3_adj!(output_x, tmp_x, x, θ_list[1], M, N, pad_x, pad_y)
+# 2d interp, 6.298 μs (2 allocations: 80 bytes)
+@btime SPECTrecon.assign_A!(A_x, vec_x)
+# 129.127 ns (0 allocations: 0 bytes)
