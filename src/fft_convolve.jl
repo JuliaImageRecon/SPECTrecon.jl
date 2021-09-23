1bd24e7e4a54b7e5dc08ce18b4aa93b77c330cce
Zongyu Li <54018575+ZongyuLi-umich@users.noreply.github.com>
2021-09-22 20:23:38 -0400

    0 allocations!

--- src/fft_convolve.jl
+++ src/fft_convolve.jl
@@ -1,39 +1,69 @@
 # fft_convolve.jl
-# up to 8x faster and more efficient than ImageFiltering.imfilter!
-import OffsetArrays
-
-fftshift!(dst::AbstractArray, src::AbstractArray) = circshift!(dst, src, div.([size(src)...],2))
-ifftshift!(dst::AbstractArray, src::AbstractArray) = circshift!(dst, src, div.([size(src)...],-2))
-function pad_it!(X::AbstractArray,
-                padsize::Tuple)
-
-    dims = size(X)
-    return OffsetArrays.no_offset_view(
-        BorderArray(X,
-            Fill(0,
-               (ceil.(Int, (padsize .- dims) ./ 2)),
-               (floor.(Int, (padsize .- dims) ./ 2)),
-            )
-        )
-    )
+"""
+    recenter2d!(dst, src)
+    the same as fftshift in 2d, but zero allocation
+"""
+function recenter2d!(dst::AbstractMatrix{<:Complex},
+                     src::AbstractMatrix{<:Complex})
+        @assert iseven(size(src, 1)) && iseven(size(src, 2))
+        m, n = div.(size(src), 2)
+        for j = 1:n, i = 1:m
+            dst[i, j] = src[m+i, n+j]
+        end
+        for j = n+1:2n, i = 1:m
+            dst[i, j] = src[m+i, j-n]
+        end
+        for j = 1:n, i = m+1:2m
+            dst[i, j] = src[i-m, j+n]
+        end
+        for j = n+1:2n, i = m+1:2m
+            dst[i, j] = src[i-m, j-n]
+        end
 end
 
+# Test code:
+# x = randn(100, 80)
+# y = similar(x)
+# z = similar(x)
+# fftshift!(y, x)
+# recenter2d!(z, x)
+# isequal(y, z)
+
+
 """
-    imfilter3!(padimg, ker, img_compl, ker_compl, fft_plan, ifft_plan)
+    imfilter3!(output, img_compl, ker, ker_compl, fft_plan, ifft_plan)
     apply FFT convolution between padimg and kernel, assuming the kernel is already centered
 """
-function imfilter3!(padimg::AbstractArray{<:Float32, 2},
-                   ker::AbstractArray{<:Float32, 2},
+function imfilter3!(output::AbstractArray{<:Real, 2},
                    img_compl::AbstractArray{Complex{T}, 2},
+                   ker::AbstractArray{<:Real, 2},
                    ker_compl::AbstractArray{Complex{T}, 2},
                    fft_plan::Union{AbstractFFTs.ScaledPlan, FFTW.cFFTWPlan},
                    ifft_plan::Union{AbstractFFTs.ScaledPlan, FFTW.cFFTWPlan}) where T <: Real
 
-    img_compl .= padimg
-    ker_compl .= pad_it!(ker, size(img_compl))
+    # ker_compl .= pad_it!(ker, size(img_compl))
+    pad2size!(ker_compl, ker, size(img_compl))
     mul!(img_compl, fft_plan, img_compl)
     mul!(ker_compl, fft_plan, ker_compl)
     img_compl .*= ker_compl
     mul!(img_compl, ifft_plan, img_compl)
-    padimg .= real.(fftshift!(ker_compl, img_compl))
+    recenter2d!(ker_compl, img_compl)
+    output .= real.(ker_compl)
 end
+
+# Test code:
+# N = 64
+# T = Float32
+# img = zeros(T, N, N)
+# img[20:50, 20:40] .= rand(31,21)
+# output = similar(img)
+# ker = ones(T, 3, 3) / 9
+# img_compl = similar(img, Complex{T})
+# ker_compl = similar(img_compl)
+# fft_plan = plan_fft!(img_compl)
+# ifft_plan = plan_ifft!(img_compl)
+# copyto!(img_compl, img)
+# @btime imfilter3!(output, img_compl, ker, ker_compl, fft_plan, ifft_plan)
+# 32.519 μs (0 allocations: 0 bytes)
+# y = imfilter(img, centered(ker))
+# plot(jim(output), jim(y), jim(output - y))
--- src/helper.jl
+++ src/helper.jl
@@ -20,21 +20,68 @@ _paddown(mumap, psfs) = floor(Int, (Power2(size(mumap, 3) + size(psfs, 2) - 1) -
 inplace version of padding a 2D image by filling zeros
 output has size (size(img, 1) + 2 * pad_x, size(img, 2) + 2 * pad_y)
 """
-function padzero!(output::AbstractMatrix{<:Real},
-                  img::AbstractMatrix{<:Real},
-                  pad_x::Int,
-                  pad_y::Int)
+Base.@propagate_inbounds function padzero!(output::AbstractMatrix{<:Real},
+                                           img::AbstractMatrix{<:Real},
+                                           pad_x::Int,
+                                           pad_y::Int)
+
         @assert size(output) == size(img) .+ (2*pad_x, 2*pad_y)
         M, N = size(img)
-        (@view output[pad_x + 1:pad_x + M, pad_y + 1:pad_y + N]) .= img
-        (@view output[1:pad_x, :]) .= 0
-        (@view output[pad_x + M + 1:end, :]) .= 0
-        (@view output[:, 1:pad_y]) .= 0
-        (@view output[:, pad_y + N + 1:end]) .= 0
+        output .= 0
+        for j = pad_y + 1:pad_y + N, i = pad_x + 1:pad_x + M
+                @inbounds output[i, j] = img[i - pad_x, j - pad_y]
+        end
+        # (@view output[pad_x + 1:pad_x + M, pad_y + 1:pad_y + N]) .= img
 end
 
 # Test code:
 # x = randn(7,5)
 # y = randn(3,3)
 # @btime padzero!(x, y, 2, 1)
-# 80.762 ns (0 allocations: 0 bytes)
+# 28.385 ns (0 allocations: 0 bytes)
+
+Base.@propagate_inbounds function pad2size!(output::AbstractMatrix{<:Complex},
+                                            img::AbstractMatrix{<:Complex},
+                                            padsize::Tuple{<:Int})
+    @assert size(output) == padsize
+    dims = size(img)
+    pad_dims = ceil.(Int, (padsize .- dims) ./ 2)
+    output .= 0
+    for j = pad_dims[2]+1:pad_dims[2]+dims[2], i = pad_dims[1]+1:pad_dims[1]+dims[1]
+        @inbounds output[i, j] = img[i - pad_dims[1], j - pad_dims[2]]
+    end
+    # (@view output[pad_dims[1]+1:pad_dims[1]+dims[1],
+    #               pad_dims[2]+1:pad_dims[2]+dims[2]]) .= img
+end
+
+function pad_it!(X::AbstractArray,
+                padsize::Tuple)
+
+    dims = size(X)
+    return OffsetArrays.no_offset_view(
+        BorderArray(X,
+            Fill(0,
+               (ceil.(Int, (padsize .- dims) ./ 2)),
+               (floor.(Int, (padsize .- dims) ./ 2)),
+            )
+        )
+    )
+end
+
+# Test code:
+# ker = randn(5,5)
+# padsize = (64, 64)
+# z = randn(padsize)
+# pad2size!(z, ker, padsize)
+# isequal(pad_it!(ker, padsize), z)
+# @btime pad2size!(z, ker, padsize)
+# 451.000 ns (0 allocations: 0 bytes)
+
+fftshift!(dst::AbstractArray{<:Real}, src::AbstractArray{<:Real}) = circshift!(dst, src, div.([size(src)...],2))
+# Test code:
+# b = [1 2;3 4]
+# x = [b 2*b;3*b 4*b]
+# y = similar(x)
+# @btime fftshift!(y, x)
+
+ifftshift!(dst::AbstractArray, src::AbstractArray) = circshift!(dst, src, div.([size(src)...],-2))
