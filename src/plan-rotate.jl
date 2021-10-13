1bbb40b3891cb9b80ddcb16f90c12cb9a55c7a0b
Jeff Fessler <JeffFessler@users.noreply.github.com>
2021-10-12 20:38:55 -0400

    Use "foreach" for 3D imrotate, document it (#17)
    * doc imrotate
    
    * overview
    
    * show, size
    
    * 3d rotate with foreach
    
    * test rotate
    
    * commit to foreach
    
    * add lmaa
    
    * fix lmaa version
    
    * RotateMode
    
    * compare :one and :two
    
    * col/row sum
    
    * 3d rotation with operator
    
    * update comment

--- src/plan-rotate.jl
+++ src/plan-rotate.jl
@@ -1,24 +1,45 @@
 # plan-rotate.jl
 
 export PlanRotate, plan_rotate
+export RotateMode, Rotate1D, Rotate2D
 using LinearInterpolators: SparseInterpolator, LinearSpline
+
+"""
+    RotateMode
+Type to control image rotation method.
+- `Rotate1D` : 3-pass 1D interpolation
+- `Rotate2D` : 1-pass 2D bilinear interpolation
+"""
+struct RotateMode{T} end
+
+"""
+    Rotate1D = RotateMode{:one}()
+Rotate using 3-pass 1D interpolation
+"""
+const Rotate1D = RotateMode{:one}()
+
+"""
+    Rotate2D = RotateMode{:two}()
+Rotate using 1-pass 2D bilinear interpolation
+"""
+const Rotate2D = RotateMode{:two}()
 
 
 """
-    PlanRotate(nx::Int; T::DataType, method::Symbol)
-Make struct for storing work arrays and factors for 2D `square` image rotation for one thread
-- `T` datatype of work arrays
-- `nx` must be Int
-- `method` interpolation methods, `:one` is to use 3-pass 1D interpolation, `:two` is to use 2D interpolation
-- `padsize` padsize, must be Int
+    PlanRotate{T, R}
+Struct for storing work arrays and factors for 2D square image rotation for one thread
+- `T` datatype of work arrays (default `Float32`)
+- `R::RotateMode`
+- `nx::Int` image size
+- `padsize::Int` : pad each side of image by this much
 - `workmat1 [nx + 2 * padsize, nx + 2 * padsize]` padded work matrix
 - `workmat2 [nx + 2 * padsize, nx + 2 * padsize]` padded work matrix
 - `workvec [nx + 2 * padsize, ]` padded work vector
-- `interp` Sparse [linear] interpolator
+- `interp::SparseInterpolator{T, 2, 1}`
 """
-struct PlanRotate{T}
+struct PlanRotate{T, R}
     nx::Int
-    method::Symbol
+#   method::Symbol
     padsize::Int
     workmat1::Matrix{T}
     workmat2::Matrix{T}
@@ -31,28 +52,24 @@ struct PlanRotate{T}
         method::Symbol = :two, # :one is for 1d interpolation, :two is for 2d interpolation
     )
 
-        @assert (method == :one || method == :two) || throw("bad method") # check interp method
+        (method == :one || method == :two) || @warn("method $method") # check interp method
 
         # only support the case that the image is square
-
         padsize = ceil(Int, 1 + nx * sqrt(2)/2 - nx / 2)
 
-        # for both 3-pass 1D and 2D interpolators:
+        # used for both 3-pass 1D and 2D interpolators:
         workmat1 = Matrix{T}(undef, nx + 2 * padsize, nx + 2 * padsize)
         workmat2 = Matrix{T}(undef, nx + 2 * padsize, nx + 2 * padsize)
 
-        # used for 1D interpolation only
+        # used for 1D interpolation only:
         # todo: look for non-allocating 1D interpolator?
-#       workvec_x = Vector{T}(undef, nx+padu_rot+padd_rot) # todo: why won't work?
-#       workvec_y = Vector{T}(undef, ny+padl_rot+padr_rot)
+#       workvec = Vector{T}(undef, nx + 2padsize) # todo: why won't work?
         workvec = zeros(T, nx + 2 * padsize) # cannot initialize as undef, otherwise interp cannot be properly initialized
 
         interp = SparseInterpolator(LinearSpline(T), workvec, length(workvec))
 
-        S = typeof(interp)
-
-        new{T}(nx,
-            method,
+        new{T, RotateMode{method}}(
+            nx,
             padsize,
             workmat1,
             workmat2,
@@ -65,19 +82,61 @@ end
 
 """
     plan_rotate(nx::Int; nthread::Int, T::DataType, method::Symbol)
-Make Vector of structs for storing work arrays and factors for 2D image rotation.
-Input
+Make `Vector` of `PlanRotate` structs
+for storing work arrays and factors for 2D square image rotation.
+
+# Input
 - `nx::Int` must equal to `ny`
-Option
+# Option
 - `T` : datatype of work arrays, defaults to `Float32`
+- `method::Symbol` : default is `:two` for 2D interpolation;
+  use `:one` for 3-pass rotation with 1D interpolation
 - `nthread::Int` # of threads, defaults to `Threads.nthreads()`
-warning: must use that default currently!
+  warning: must use that default currently!
 """
 function plan_rotate(
     nx::Int ;
     nthread::Int = Threads.nthreads(),
     T::DataType = Float32,
     method::Symbol = :two,
-    )
+)
     return [PlanRotate(nx; T, method) for id = 1:nthread]
+end
+
+
+"""
+    show(io::IO, ::MIME"text/plain", plan::PlanRotate)
+"""
+function Base.show(io::IO, ::MIME"text/plain", plan::PlanRotate{T,R}) where {T, R}
+    t = typeof(plan)
+    println(io, t)
+    for f in (:nx, :padsize)
+        p = getproperty(plan, f)
+        t = typeof(p)
+        println(io, " ", f, "::", t, " ", p)
+    end
+    for f in (:interp, :workmat1, :workmat2, :workvec)
+        p = getproperty(plan, f)
+        println(io, " ", f, ":", " ", summary(p))
+    end
+    println(io, " (", sizeof(plan), " bytes)")
+end
+
+
+"""
+    show(io::IO, mime::MIME"text/plain", vp::Vector{<:PlanRotate})
+"""
+function Base.show(io::IO, mime::MIME"text/plain", vp::Vector{PlanRotate{T,R}}) where {T,R}
+    t = typeof(vp)
+    println(io, length(vp), "-element ", t, " with N=", vp[1].nx)
+#   show(io, mime, vp[1])
+end
+
+
+"""
+    sizeof(::PlanRotate)
+Show size in bytes of `PlanRotate` object.
+"""
+function Base.sizeof(ob::T) where {T <: Union{PlanRotate, SparseInterpolator}}
+    sum(f -> sizeof(getfield(ob, f)), fieldnames(typeof(ob)))
 end
